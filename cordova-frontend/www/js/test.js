c3e295d0286ff5dc5a84375d55400d06952e09f7
MachinaDeus <Domedagen42@gmail.com>
2016-04-22 17:06:59 +0200

    Got a list that is updates when you press get database button, and which is linked to the play button (no more radio) so that it will play the assoiseted objekts url.

--- cordova-frontend/www/js/test.js
+++ cordova-frontend/www/js/test.js
@@ -1,48 +1,43 @@
 /**
  * Created by carl-johanlindblad on 2016-04-19.
  */
-var fetchStuffFromDB = function() {
 
-    // bara för test, det här borde göras med jQuery istället
-    var request = new XMLHttpRequest();
-    request.open("GET", "https://satans-demokrati-72.herokuapp.com/test", true);
-    request.onreadystatechange = function() {
-        if (request.readyState == 4) {
-            if (request.status == 200 || request.status == 0) {
-                var result = JSON.parse(request.responseText);
-                console.log(result);
-                var data = "<table cellspacing='0'>";
-                for (i = 0; i < result.length; i++) {
-                    data += "<tr style='border: 1px solid black'>";
-                    data += "<td>";
+var myaudioURL = null;
 
-                    // Json key
-                    data += result[i].name;
+function fetchStuffFromDB() {
+        $.getJSON("https://satans-demokrati-72.herokuapp.com/test", function(radios){
+                // empty List
+        $('#radioList').empty();
 
-                    data += "</td>";
-                    data += "</tr>";
-                    data += "<tr>";
-                    data += "<td>";
+                //add to list
+        $.each(radios, function (i, radio) {
+            $('#radioList').append(generateRadioLink(radio));
+        });
+            // refresh list ( Seem to not do anything atm)
+        $('#radioList').listview("refresh");
 
-                    // Json key
-                    data += '<a href="' + result[i].filepath + '">' + result[i].filepath + '</a>';
+            });
 
-                    data += "</td>";
-                    data += "</tr>"
-                }
-                data += "</table>";
-                var div = document.getElementById("DBStuffz");
-                div.innerHTML = data;
-
-            }
-        }
-    }
-    request.send();
 
 }
+function generateRadioLink(radio){
+    // Suppose to make the object into proper format, Not sure if working
+    return '<li><a href="javascript:void(0)'
+            +'" onclick="swapRadio(\' '
+            + radio.name
+            + '\',\''
+            + radio.filepath + '\')">'
+            + radio.name
+            + '</a></li>';
+}
+
+function swapRadio(radioName, filepath){
+    myaudioURL = filepath;
+}
+
 var playStream =  function () {
     try {
-        var myaudioURL = 'http://stream.4zzzfm.org.au:789/;';
+       // var myaudioURL = 'http://stream.4zzzfm.org.au:789/;';
         var myaudio = new Audio(myaudioURL);
         myaudio.id = 'playerMyAdio';
         myaudio.play();
