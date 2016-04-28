56e92084b5b94fd717900615af862f5c0e69c22b
Elias <elias.c.lindgren@gmail.com>
2016-04-28 10:39:31 +0200

    new

--- app/models/Sound.java
+++ app/models/Sound.java
@@ -1,14 +1,37 @@
 package models;
 import com.avaje.ebean.Model;
+import com.sun.org.apache.xpath.internal.operations.String;
+
 import javax.persistence.*;
 
 @Entity
 public class Sound extends Model {
 
+    public String tower;
     public String character;
-    public String ;
+    public String soundLink;
+    public String message;
 
+    public Sound(String tower, String character, String soundLink, String message){
+        this.tower = tower;
+        this.character = character;
+        this.soundLink = soundLink;
+        this.message = message;
+    }
 
     @Id
     public int ID;
+
+    public String getTower(){ return tower; }
+
+    public void setTower(String tower){ this.tower = tower; };
+
+    public String getCharacter(){ return character; };
+
+    public void setCharacter(){ this.character = character; };
+
+    public String getSoundLink(){ return soundLink; };
+
+    public void setSoundLink(){ this.soundLink = soundLink; };
+
 }
