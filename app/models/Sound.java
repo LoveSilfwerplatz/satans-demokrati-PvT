704c12dbf9ad83ad7246d4b98b3e7fbbc33d0e43
Elias <elias.c.lindgren@gmail.com>
2016-04-28 11:48:37 +0200

    outcommented shit

--- app/models/Sound.java
+++ app/models/Sound.java
@@ -5,6 +5,8 @@ package models;
  */
 
 import com.avaje.ebean.Model;
+import com.sun.org.apache.xpath.internal.operations.String;
+
 import javax.persistence.*;
 
 public class Sound extends Model {
@@ -12,3 +14,46 @@ public class Sound extends Model {
     public String name;
     //ftp
 }
+/*
+package models;
+        import com.avaje.ebean.Model;
+        import com.sun.org.apache.xpath.internal.operations.String;
+
+        import javax.persistence.*;
+
+@Entity
+public class Sound extends Model {
+
+    public String tower;
+    public String character;
+    public String soundLink;
+    public String message;
+
+    public Sound(String tower, String character, String soundLink, String message){
+        this.tower = tower;
+        this.character = character;
+        this.soundLink = soundLink;
+        this.message = message;
+    }
+
+    @Id
+    public int ID;
+
+    public String getTower(){ return tower; }
+
+    public void setTower(String tower){ this.tower = tower; };
+
+    public String getCharacter(){ return character; };
+
+    public void setCharacter(String character){ this.character = character; };
+
+    public String getSoundLink(){ return soundLink; };
+
+    public void setSoundLink(String soundLink){ this.soundLink = soundLink; };
+
+    public String getMessage(){ return message; };
+
+    public void setMessage(String message){ this.message = message; };
+
+}
+*/
