4850c490683b34f0768db913cc472f9108479538
Elias <elias.c.lindgren@gmail.com>
2016-04-28 10:21:44 +0200

    new

---
+++ app/models/Sound.java
@@ -0,0 +1,14 @@
+package models;
+import com.avaje.ebean.Model;
+import javax.persistence.*;
+
+@Entity
+public class Sound extends Model {
+
+    public String character;
+    public String ;
+
+
+    @Id
+    public int ID;
+}
