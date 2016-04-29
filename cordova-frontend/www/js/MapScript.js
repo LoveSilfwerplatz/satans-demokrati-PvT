5e9d3277e2a4dd6f5320450a5f44dba337e265c4
david <david.karlsson.lille@live.se>
2016-04-29 11:49:54 +0200

    Geolocation fungerar, ikon och konstant uppdatering(GPS liknande behöverbeteende med konstanta uppdateringer) behövs i framtiden)

--- cordova-frontend/www/js/MapScript.js
+++ cordova-frontend/www/js/MapScript.js
@@ -5,9 +5,35 @@
 //Only Temp Script, Should probly be combined later when doing the html dynamically
 
 function initMap() {
-    var mapDiv = document.getElementById('map')
-    var map = new google.maps.Map(mapDiv, {
-        center: {lat: 44.540, lng: -78.546},
-        zoom: 21
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 1, lng: 10},
+        zoom: 15
     });
+    var infoWindow = new google.maps.InfoWindow({map: map});
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
 }
