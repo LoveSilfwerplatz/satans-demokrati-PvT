4a615f11ebfa3eb737ddc7e0ea5a487189eca3f2
Elias <elias.c.lindgren@gmail.com>
2016-05-04 02:08:11 +0200

    Works now, but ugly

--- app/views/JavaScript/Scripts.js
+++ app/views/JavaScript/Scripts.js
@@ -3,43 +3,20 @@ var play_url = "http://localhost:9000";
 $(document).ready(function() {
 
 });
-    
-/*function fetch() {
-    $.getJSON(play_url + "/getTowers", function (towers) {
-        // empty List
-        $('#towerList').empty();
-
-        //add to list
-        $.each(towers, function (name) {
-            $('#towerList').append(populate(name));
-        });
-
-
-    });
-}*/
 
 function populate() {
 
-/*    $("#text-one").change(function() {
-        $("#text-two").load("textdata/" + $(this).val() + ".txt");
-    });
-
-    $("#json-one").change(function() {*/
-
-
     $.getJSON(play_url + "/getTowers", function (towers) {
-            alert("FUNKAR!!!");
-            var vals = [];
 
-            vals = towers;
 
-            var $towerDrop = $("#towerDrop");
-            $towerDrop.empty();
-            $.each(vals, function(lat, long, ID, name, id, range) {
-                $towerDrop.append("<option>" + name + "</option>");
+        var $towerDrop = $("#towerDrop");
+        $towerDrop.empty();
+        $.each(towers, function(index , value) {
+            $.each(value, function(id, name){
+                if(id == "name") {
+                    $towerDrop.append("<option>" + name + "</option>");
+                }
             });
-
         });
-   // });
-
+    });
 };
