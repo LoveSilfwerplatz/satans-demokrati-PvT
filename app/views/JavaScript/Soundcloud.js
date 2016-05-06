5e8ddf794491ba33a487c544b1042b57256db492
Theo Walther <woebin@gmail.com>
2016-05-06 16:04:14 +0200

    added Soundcloud.js and sctest.html in which I'm working on Soundcloud implementation, far from functional right now (but authentication seems to work, I think?)

---
+++ app/views/JavaScript/Soundcloud.js
@@ -0,0 +1,69 @@
+/**
+ * Created by Theo on 06/05/2016.
+ */
+var debug = true;
+var play_url = debug ? "http://localhost:9000" : "https://satans-demokrati-72.herokuapp.com";
+
+SC.initialize({
+    client_id: '6a0f1d47b7df82417d31a6947ab0032c',
+    redirect_uri: 'http://googledrive.com/host/0B636f7sGUTKTSFlrNDNfNnVSTGs'    // This is the Horrible Temporary Hosting Test in Drive, please don't destroy it.
+});
+
+// initiate auth popup. Supposed to autoclose after authenticating, but it's not doing that. Unsure why right now, probably some dumb mistake on my part.
+SC.connect().then(function() {
+    return SC.get('/me');
+}).then(function(me) {
+    alert('Hello, ' + me.username);
+});
+
+// Stuff below is basically taken verbatim from Codecademy course, it's really no more than a placeholder right now.
+$(document).ready(function() {
+    $('#startRecording a').click(function(e){
+        updateTimer(0);
+        $('#startRecording').hide();
+        $('#startRecording').show();
+        e.preventDefault();
+        SC.record({
+            progress: function(ms, avgPeak){
+                updateTimer(ms)
+            }
+        });
+    });
+    $('#stopRecording a').click(function(e){
+        SC.recordStop;
+        $('#playBack').show();
+        $('#upload').show();
+        $('#stopRecording').hide();
+        e.preventDefault();
+    });
+    $('#playBack a').click(function(e){
+        e.preventDefault();
+        updateTimer(0);
+        SC.recordPlay({
+            progress: function(ms){
+                updateTimer(ms);
+            }
+        });
+    });
+    $('#upload a').click(function(e){
+        e.preventDefault();
+        SC.connect({
+            connected: function(){
+                SC.upload({
+                    track: {
+                        file: "fileGoesHere",
+                        title: "My Codecademy recording",
+                        sharing: "private"
+                    }
+                })
+            }
+        })
+    })
+});
+
+// Helper methods for our UI.
+
+function updateTimer(ms) {
+    // update the timer text. Used when we're recording
+    $('.status').text(SC.Helper.millisecondsToHMS(ms));
+}
