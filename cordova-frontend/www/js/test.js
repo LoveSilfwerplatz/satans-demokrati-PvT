021879222e194624747fdc7929e908133b2697b1
cali3943 <carljohan.lindblad@gmail.com>
2016-05-10 12:21:35 +0200

    Links to audio and map added

--- cordova-frontend/www/js/test.js
+++ cordova-frontend/www/js/test.js
@@ -66,7 +66,7 @@ $(document).ready(function() {
 
 
 // set to true for local play framework development
-var debug = false;
+var debug = true;
 var play_url = debug ? "http://localhost:9000" : "https://satans-demokrati-72.herokuapp.com";
 
 var myaudio = new Audio();
